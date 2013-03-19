2a63815e341998a333b9d38b8e8127a21287350a
David Cramer <dcramer@gmail.com>
2013-03-18 21:12:23 -0700

    0.5.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='logan',
-    version='0.5.4',
+    version='0.5.5',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/logan',
