c99b4e57ebd7e40a81cf2d8240b054c478e045be
David Cramer <dcramer@gmail.com>
2013-09-19 01:44:17 -0700

    0.5.8.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='logan',
-    version='0.5.8.1',
+    version='0.5.8.2',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/logan',
