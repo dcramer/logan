8b18456802d631a822e2823bf9a4e9810a15a20e
Odin Hørthe Omdal <odin.omdal@gmail.com>
2016-05-18 15:56:28 +0200

    0.7.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='logan',
-    version='0.7.1',
+    version='0.7.2',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/logan',
